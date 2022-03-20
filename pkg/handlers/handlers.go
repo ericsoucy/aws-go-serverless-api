5faba0475be3810951a14f99076bae0a2f9c6d5a
eric soucy <eric.soucy@gmail.com>
2022-03-20 17:11:44 -0400

    handler 1st few commits

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -16,7 +16,20 @@ var ErrorBody struct {
 
 func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)
             (*events.APIGatewayProxyResponse, error){
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetchUser(email, tableName, dynaClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusOK, result)
+	}
 
+	result, err := user.FetchUsers(tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+	}
+	return apiResponse(http.StatusOK, result)
 }
 
 func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)
